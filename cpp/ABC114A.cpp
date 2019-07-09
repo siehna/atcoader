eefaa391fd368e295d04827dd202e4c9fa057335
siehna <bq13026@gmail.com>
2019-07-09 22:22:35 +0900

    フォルダ内整理

---
+++ cpp/ABC114A.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // 整数の入力
+    int a;
+     cin >> a;
+    if(a==7){
+         cout  << "YES\n " << endl;
+        return 0;
+    }else if(a==5){
+         cout  << "YES\n " << endl;
+        return 0;
+    }else if(a==3){
+         cout  << "YES\n " << endl;
+        return 0;
+    }
+    // 出力
+    cout  << "NO\n " << endl;
+    return 0;
+}
